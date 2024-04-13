99d95998dd822295d865b7bbeb9cf691d00dfb47
FabricioRodriguesZ <166947713+FabricioRodriguesZ@users.noreply.github.com>
2024-04-13 16:15:52 -0300

    Create Post.java

---
+++ java/models/Post.java
@@ -0,0 +1,58 @@
+package com.example.mobilen1.models;
+
+import java.util.List;
+
+public class Post {
+    private int id;
+    private int userId;
+    private String title;
+    private String body;
+    private List<Comment> comments; // Adicionando uma lista de comentários
+
+    public Post(int id, int userId, String title, String body) {
+        this.id = id;
+        this.userId = userId;
+        this.title = title;
+        this.body = body;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+}
